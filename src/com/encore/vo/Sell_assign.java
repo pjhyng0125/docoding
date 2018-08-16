2d9a19623872077c7ad9d6051c92afb4ad774a21
park <pjhyng0125@naver.com>
2018-08-16 10:22:40 +0900

    pjh
    up Sell_assign.java

--- src/com/encore/vo/Sell_assign.java
+++ src/com/encore/vo/Sell_assign.java
@@ -6,17 +6,15 @@ public class Sell_assign {
 	private String id;
 	private String account_num;
 	private Date sa_time;
-	private String sale_flag;
 	private int sell_count;
 	public Sell_assign() {
 		super();
 	}
-	public Sell_assign(String id, String account_num, Date sa_time, String sale_flag, int sell_count) {
+	public Sell_assign(String id, String account_num, Date sa_time, int sell_count) {
 		super();
 		this.id = id;
 		this.account_num = account_num;
 		this.sa_time = sa_time;
-		this.sale_flag = sale_flag;
 		this.sell_count = sell_count;
 	}
 	public String getId() {
@@ -37,12 +35,6 @@ public class Sell_assign {
 	public void setSa_time(Date sa_time) {
 		this.sa_time = sa_time;
 	}
-	public String getSale_flag() {
-		return sale_flag;
-	}
-	public void setSale_flag(String sale_flag) {
-		this.sale_flag = sale_flag;
-	}
 	public int getSell_count() {
 		return sell_count;
 	}
@@ -51,10 +43,9 @@ public class Sell_assign {
 	}
 	@Override
 	public String toString() {
-		return "Sell_assign [id=" + id + ", account_num=" + account_num + ", sa_time=" + sa_time + ", sale_flag="
-				+ sale_flag + ", sell_count=" + sell_count + "]";
+		return "Sell_assign [id=" + id + ", account_num=" + account_num + ", sa_time=" + sa_time + ", sell_count="
+				+ sell_count + "]";
 	}
-	
 	
 }
 
