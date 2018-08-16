50180403f3cf6fc07d0008ea6c77643e2822ecd5
park <pjhyng0125@naver.com>
2018-08-16 10:21:34 +0900

    pjh
    up Member.java

--- src/com/encore/vo/Member.java
+++ src/com/encore/vo/Member.java
@@ -11,93 +11,15 @@ public class Member {
 	private String birth;
 	private String email;
 	private String phone;
-	private boolean login;
+	private boolean login_flag;
 	private Date m_time;
 	
 	public Member() {
-		// TODO Auto-generated constructor stub
+		super();
 	}
-
-	@Override
-	public String toString() {
-		return "Member [id=" + id + ", pass=" + pass + ", name=" + name + ", gender=" + gender + ", birth=" + birth
-				+ ", email=" + email + ", phone=" + phone + ", login=" + login + ", m_time=" + m_time + "]";
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getPass() {
-		return pass;
-	}
-
-	public void setPass(String pass) {
-		this.pass = pass;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getGender() {
-		return gender;
-	}
-
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-
-	public String getBirth() {
-		return birth;
-	}
-
-	public void setBirth(String birth) {
-		this.birth = birth;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
-	public boolean isLogin() {
-		return login;
-	}
-
-	public void setLogin(boolean login) {
-		this.login = login;
-	}
-
-	public Date getM_time() {
-		return m_time;
-	}
-
-	public void setM_time(Date m_time) {
-		this.m_time = m_time;
-	}
-
+	
 	public Member(String id, String pass, String name, String gender, String birth, String email, String phone,
-			boolean login, Date m_time) {
+			boolean login_flag, Date m_time) {
 		super();
 		this.id = id;
 		this.pass = pass;
@@ -106,8 +28,73 @@ public class Member {
 		this.birth = birth;
 		this.email = email;
 		this.phone = phone;
-		this.login = login;
+		this.login_flag = login_flag;
 		this.m_time = m_time;
+	}
+
+
+
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public boolean isLogin_flag() {
+		return login_flag;
+	}
+	public void setLogin_flag(boolean login_flag) {
+		this.login_flag = login_flag;
+	}
+	public Date getM_time() {
+		return m_time;
+	}
+	public void setM_time(Date m_time) {
+		this.m_time = m_time;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Member [id=" + id + ", pass=" + pass + ", name=" + name + ", gender=" + gender + ", birth=" + birth
+				+ ", email=" + email + ", phone=" + phone + ", login_flag=" + login_flag + ", m_time=" + m_time + "]";
 	}
 	
 	
