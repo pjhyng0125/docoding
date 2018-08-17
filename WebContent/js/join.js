5d5d74c14b8539a01e6ac2e34b1e8317b8ef7a2e
jihwan9905 <40711645+jihwan9905@users.noreply.github.com>
2018-08-17 19:12:24 +0900

    지환

--- WebContent/js/join.js
+++ WebContent/js/join.js
@@ -1,4 +1,5 @@
-	function checkValue() {
+//----------------join 시작-------------------	
+function checkValue() {
 		var form = document.userInfo;
 
 		 if (!form.id.value) {
@@ -66,4 +67,84 @@
 	function inputIdChk() {
 		document.userInfo.idDuplication.value = "idUncheck";
 	}
-    
+    //---------------------join마침-----------------
+	
+	//----------------------idcheck시작---------------    
+	$(function(){
+		$('[name=idcheck]').click(function(){
+			$.ajax({
+				url:'/docoding/MemberAction.do',
+				data:{
+					userid:$('[name=idinput]').val(),
+				},
+				success:function(result){
+					$('#wrap').html(result);
+				}
+				
+			});		
+		});		
+
+		
+	});
+     
+     
+     // 회원가입창의 아이디 입력란의 값을 가져온다.
+     function pValue(){
+         document.getElementById("userId").value = opener.document.userInfo.id.value;
+     }
+     
+     // 아이디 중복체크
+     function idCheck(){
+
+         var id = document.getElementById("userId").value;
+
+         if (!id) {
+             alert("아이디를 입력하지 않았습니다.");
+             return false;
+         } 
+         else if((id < "0" || id > "9") && (id < "A" || id > "Z") && (id < "a" || id > "z")){ 
+             alert("한글 및 특수문자는 아이디로 사용하실 수 없습니다.");
+             return false;
+         }
+         else
+         {
+             var param="id="+id
+             httpRequest = getXMLHttpRequest();
+             httpRequest.onreadystatechange = callback;
+             httpRequest.open("POST", "/docoding/idCheck.do", true);    
+             httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded'); 
+             httpRequest.send(param);         
+         }
+     }
+     
+     function callback(){
+         if(httpRequest.readyState == 4){
+             // 결과값을 가져온다.
+             var resultText = httpRequest.responseText;
+             if(resultText == 0){
+                 alert("사용할수없는 아이디입니다.");
+                 document.getElementById("cancelBtn").style.visibility='visible';
+                 document.getElementById("useBtn").style.visibility='hidden';
+                 document.getElementById("msg").innerHTML ="";
+             } 
+             else if(resultText == 1){ 
+                 document.getElementById("cancelBtn").style.visibility='hidden';
+                 document.getElementById("useBtn").style.visibility='visible';
+                 document.getElementById("msg").innerHTML = "사용 가능한 아이디입니다.";
+             }
+         }
+     }
+     
+
+     function sendCheckValue(){
+    	// 중복체크 결과인 idCheck 값을 전달한다.
+         opener.document.userInfo.idDuplication.value ="idCheck";
+        // 회원가입 화면의 ID입력란에 값을 전달
+         opener.document.userInfo.id.value = document.getElementById("userId").value;
+         if (opener != null) {
+             opener.chkForm = null;
+             self.close();
+         }    
+     }    
+	
+	//----------------------idcheck마침------------------------
