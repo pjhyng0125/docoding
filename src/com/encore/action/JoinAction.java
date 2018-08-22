b9c88e1d6ef8124b37454e1e98dad8b80be50388
park <pjhyng0125@naver.com>
2018-08-22 16:00:21 +0900

    pjh
    up

--- src/com/encore/action/JoinAction.java
+++ src/com/encore/action/JoinAction.java
@@ -1,5 +1,18 @@
 package com.encore.action;
 
-public class JoinAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+public class JoinAction extends Action {
+	@Override
+	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		System.out.println("execute()...");
+		return mapping.findForward("joinpage");
+	}
 }
